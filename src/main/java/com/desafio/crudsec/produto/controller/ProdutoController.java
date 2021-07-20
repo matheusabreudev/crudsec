e57afaa194127d974d3513d93393238a03069437
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-07-20 02:06:49 -0300

    Adicionado o GET para busca de um unico produto

--- src/main/java/com/desafio/crudsec/produto/controller/ProdutoController.java
+++ src/main/java/com/desafio/crudsec/produto/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class ProdutoController {
 	@GetMapping("/produtos")
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produto/{id}")
+	public Produto listaProduto(@PathVariable(value="id") long id){
+		return produtoRepository.findById(id);
 	}
 	
 }
--- src/main/java/com/desafio/crudsec/produto/repo/ProdutoRepository.java
+++ src/main/java/com/desafio/crudsec/produto/repo/ProdutoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.desafio.crudsec.produto.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
+	
+	Produto findById(long id);
+	
 }
