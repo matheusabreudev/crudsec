7e22f7913b0ed8d36e3a3afb099fcc2d78a30579
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-07-20 01:53:39 -0300

    Adicionado o metodo GET para todos os produtos

--- src/main/java/com/desafio/crudsec/produto/controller/ProdutoController.java
+++ src/main/java/com/desafio/crudsec/produto/controller/ProdutoController.java
@@ -1,8 +1,25 @@
 package com.desafio.crudsec.produto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ProdutoController {
+import com.desafio.crudsec.produto.model.Produto;
+import com.desafio.crudsec.produto.repo.ProdutoRepository;
 
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoController {
+	
+	@Autowired
+	ProdutoRepository produtoRepository ;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos(){
+		return produtoRepository.findAll();
+	}
+	
 }
